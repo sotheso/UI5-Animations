fa89449ff5cece4a7af5c54cd55282ae43860696
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-08 19:39:13 +0330

    Initial Commit

---
+++ UI5-Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UI5-Animations
+//
+//  Created by Sothesom on 18/09/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UI5-Animations/UI5_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI5_AnimationsApp.swift
+//  UI5-Animations
+//
+//  Created by Sothesom on 18/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct UI5_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
